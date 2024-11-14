23cfa9dea2ca8ceeb8899f1aadf4b5eefaca4d92
M4deN <alexdesaran@outlook.com>
2024-11-14 16:15:32 -0300

    Update playground.cy.js

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,7 +1,20 @@
 describe('Cypress Playground Tests', () => {
-  it('Visit Banner Cyporess Cloud Course', () => {
+
+  beforeEach(()=>{
     cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
-    cy.get('#promotional-banner').contains('📣 Get to know the')
-    cy.get('a[target="_blank"]').contains('Cypress, from Zero to the Cloud')
+  })
+
+  it('Shows a Banner Cypress Cloud Course', () => {
+    cy.get('#promotional-banner').should('be.visible')
+    cy.contains('Cypress, from Zero to the Cloud').should('be.visible').click()
+  })
+  it('Click the Subscribe and shows a message', () => {
+    cy.contains('button','Subscribe').should('be.visible').click()
+    cy.contains('#success',"You've been successfully subscribed to our newsletter.").should('be.visible')
+  })
+  it('Types i an input wich "Signs"a form then asserts it is signed',()=>{
+    cy.get('#signature-textarea').type('Leandro A. Medeiros')
+    cy.get('#signature').should('have.text','Leandro A. Medeiros')
+    cy.contains('#signature','Leandro A. Medeiros').should('be.visible')
   })
 })
