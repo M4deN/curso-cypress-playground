715d3e17444b322fda2d89c1fc12519ad3b781a5
M4deN <alexdesaran@outlook.com>
2024-11-21 11:38:41 -0300

    Update playground.cy.js

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -17,4 +17,28 @@ describe('Cypress Playground Tests', () => {
     cy.get('#signature').should('have.text','Leandro A. Medeiros')
     cy.contains('#signature','Leandro A. Medeiros').should('be.visible')
   })
+
+  it('Types in the signature fields, check the checkbox to see the preview the asserts in the sign',()=>{
+    cy.get('#signature-textarea-with-checkbox').type('Leandro A. Medeiros Sign New')
+    cy.get('#signature-checkbox').check()
+    cy.get('#signature-triggered-by-check').should('have.text','Leandro A. Medeiros Sign New')
+    cy.contains('#signature-triggered-by-check','Leandro A. Medeiros Sign New').should('be.visible')
+    cy.get('#signature-checkbox').uncheck()
+    cy.contains('#signature-triggered-by-check','Leandro A. Medeiros Sign New').should('not.exist')
+  })
+
+  it.only('Check both possible Radios and asserts if it is "on"  or "off" ',()=>{
+    cy.get('#on-off').should('have.text','ON')
+    cy.contains('#off').should('not.exist')
+    cy.get('#off').check()
+    cy.get('#on-off').should('have.text','OFF')
+    cy.get('#on-off').should('have.text','OFF')
+    cy.get('#on-off').should('be.visible','OFF').should('be.visible')
+    cy.contains('#on').should('not.exist')
+    cy.get('#on').check()
+    cy.contains('#off').should('not.exist')
+    cy.get('input[type="radio"][value="on"]').check()
+    cy.get('#on-off').should('have.text','ON')
+    cy.get('#on-off').should('be.visible','ON ').should('be.visible')
+  })
 })
